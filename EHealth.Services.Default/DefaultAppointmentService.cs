b4d45d13f53754462373114d90b90be16dc2a6b6
Mihailo <mihailpro02@gmail.com>
2022-05-27 20:15:59 +0300

    Added services w/o logic

---
+++ EHealth.Services.Default/DefaultAppointmentService.cs
@@ -0,0 +1,24 @@
+﻿using EHealth.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHealth.Services.Default
+{
+    public class DefaultAppointmentService : IAppointmentService
+    {
+        public async Task<IEnumerable<HistoryModel>> GetHistoryForPatientAsync(string patientFullName)
+        {
+            await Task.CompletedTask;
+            return Array.Empty<HistoryModel>();
+        }
+
+        public async Task<bool> ScheduleAppointmentAsync(AppointmentModel appointmentModel)
+        {
+            await Task.CompletedTask;
+            return false;
+        }
+    }
+}
---
+++ EHealth.Services.Default/DefaultDoctorsService.cs
@@ -0,0 +1,22 @@
+﻿using EHealth.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EHealth.Services.Default
+{
+    public class DefaultDoctorsService : IDoctorsService
+    {
+        public async Task<DoctorModel> GetDoctorAsync(int id)
+        {
+            await Task.CompletedTask;
+            return null;
+        }
+
+        public async Task<IEnumerable<DoctorModel>> GetDoctorsListAsync()
+        {
+            await Task.CompletedTask;
+            return Array.Empty<DoctorModel>();
+        }
+    }
+}
