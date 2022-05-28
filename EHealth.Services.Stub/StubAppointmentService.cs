29dd4b317e7a3d4251f75a620991de032d4b85c4
Mihailo <mihailpro02@gmail.com>
2022-05-28 17:12:27 +0300

    Stubbed BL services

---
+++ EHealth.Services.Stub/StubAppointmentService.cs
@@ -0,0 +1,37 @@
+﻿using EHealth.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EHealth.Services.Stub
+{
+    public class StubAppointmentService : IAppointmentService
+    {
+        public async Task<IEnumerable<HistoryModel>> GetHistoryForPatientAsync(string patientFullName)
+        {
+            return await Task.FromResult(new List<HistoryModel>()
+            {
+                new()
+                {
+                    PatientFullName = "Mihailo P",
+                    DoctorFullName = "Robert D",
+                    AppointmentDateTime = new DateTime(2022, 06, 01, 17, 00, 00),
+                    Status = "Completed",
+                    Diagnosis = "Sleep deprivation",
+                },
+                new()
+                {
+                    PatientFullName = "Mihailo P",
+                    DoctorFullName = "Halyatin V",
+                    AppointmentDateTime = new DateTime(2022, 06, 20, 15, 00, 00),
+                    Status = "Pending",
+                }
+            });
+        }
+
+        public async Task<bool> ScheduleAppointmentAsync(AppointmentModel appointmentModel)
+        {
+            return await Task.FromResult(true);
+        }
+    }
+}
---
+++ EHealth.Services.Stub/StubDoctorsService.cs
@@ -0,0 +1,70 @@
+﻿using EHealth.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EHealth.Services
+{
+    public class StubDoctorsService : IDoctorsService
+    {
+        public async Task<DoctorModel> GetDoctorAsync(int id)
+        {
+            return await Task.FromResult(new DoctorModel()
+            {
+                Id = 0,
+                FullName = "Vladimir H",
+                Occupations = new string[] { "Therapist", "Ph.D" },
+                AvailableAppointmentTime = new DateTime[]
+                    {
+                        new DateTime(2022, 07, 01, 13, 00, 00),
+                        new DateTime(2022, 07, 01, 15, 00, 00),
+                        new DateTime(2022, 07, 01, 17, 00, 00),
+                    }
+            });
+        }
+
+        public async Task<IEnumerable<DoctorModel>> GetDoctorsListAsync()
+        {
+            return await Task.FromResult(new List<DoctorModel>()
+            {
+                new()
+                {
+                    Id = 0,
+                    FullName = "Vladimir H",
+                    Occupations = new string[] { "Therapist", "Ph.D" },
+                    AvailableAppointmentTime = new DateTime[] 
+                    { 
+                        new DateTime(2022, 07, 01, 13, 00, 00),
+                        new DateTime(2022, 07, 01, 15, 00, 00),
+                        new DateTime(2022, 07, 01, 17, 00, 00),
+                    }
+                },
+                new()
+                {
+                    Id = 1,
+                    FullName = "Robert D",
+                    Occupations = new string[] { "Psychologist", "Neurologist" },
+                    AvailableAppointmentTime = new DateTime[]
+                    {
+                        new DateTime(2022, 07, 01, 17, 00, 00),
+                        new DateTime(2022, 07, 01, 17, 30, 00),
+                        new DateTime(2022, 07, 01, 18, 00, 00),
+                        new DateTime(2022, 07, 01, 18, 30, 00),
+                    }
+                },
+                new()
+                {
+                    Id = 2,
+                    FullName = "Ivan K",
+                    Occupations = new string[] { "Therapist" },
+                    AvailableAppointmentTime = new DateTime[]
+                    {
+                        new DateTime(2022, 07, 02, 13, 00, 00),
+                        new DateTime(2022, 07, 02, 15, 00, 00),
+                        new DateTime(2022, 07, 02, 17, 00, 00),
+                    }
+                }
+            });
+        }
+    }
+}
